b411ca23be57c4fe6b059306eed7f23305b2cc27
jarra <suhrawardi@gmail.com>
2021-04-24 15:02:17 +0200

    Playing around a bit

--- src/Complexity.jl
+++ src/Complexity.jl
@@ -1,6 +1,11 @@
 module Complexity
   using Plots
 
+  function fun1(x)
+    b = 1.1
+    return x * 1.1 + b
+  end
+
   function fun2(x)
     b = 1.9
     return x * 1.1 + b
@@ -10,11 +15,16 @@ module Complexity
     return (m, x) -> append!(m, isempty(m) ? fun(x) : fun(last(m)))
   end
 
-  function plt(fn, xs)
-    plot(xs, reduce(func(fn), xs; init=Float64[]))
+  function plt(fn, xs; title="", label=[])
+    return plot(xs, reduce(func(fn), xs; init=Float64[]), title=title, label=label)
+  end
+
+  function plt!(fn, p, xs; title="", label=[])
+    return plot!(p, xs, reduce(func(fn), xs; init=Float64[]), title=title, label=label)
   end
 
   function main()
-    plt(fun2, 1:30)
+    p = plt(fun1, 1:30, label=["A"])
+    plt!(fun2, p, 1:30, label=["B"])
   end
 end
